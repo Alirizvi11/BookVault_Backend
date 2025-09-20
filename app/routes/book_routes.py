545b9688a23835085b2254c650fec10ec1d80e95
Ali Rizvi <125128547+Alirizvi11@users.noreply.github.com>
2025-09-20 10:55:19 +0530

    Update book_routes.py

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,23 +1,27 @@
 from flask import Blueprint, jsonify
+from flask_cors import cross_origin
 from app.db_config import get_connection
 
 book_bp = Blueprint("book_bp", __name__, url_prefix="/books")
 
-@book_bp.route("/get-books", methods=["GET"])
+@book_bp.route("/get-books", methods=["GET", "OPTIONS"])
+@cross_origin(origins=[
+    'https://demobookvaultui.vercel.app',
+    'https://demobookvault.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
+])
 def get_books():
     conn = None
     cursor = None
-
     try:
         conn = get_connection()
         cursor = conn.cursor()
-
         cursor.execute("""
             SELECT book_id, title, author, genre, available_copies, cover_url
             FROM books
         """)
         rows = cursor.fetchall()
-
         books = [
             {
                 "book_id": row[0],
@@ -29,13 +33,10 @@ def get_books():
             }
             for row in rows
         ]
-
         return jsonify(books)
-
     except Exception as e:
         print("❌ GetBooks error:", e)
         return jsonify({"error": str(e)}), 500
-
     finally:
         if cursor: cursor.close()
         if conn: conn.close()
